3c5b80967cab9558aa5c894b8fda43e2df54208c
Alejandro <Alejandro@LAPTOP-PO165527>
2023-05-23 12:25:24 +0200

    Clase ComparadorPrecio (Codigo ComparadorPrecio)

---
+++ PracticaT8T10_Grupo3/src/Comparador/ComparadorPrecio.java
@@ -0,0 +1,20 @@
+package Comparador;
+
+import java.util.Comparator;
+
+import videojuego.Videojuego;
+
+public class ComparadorPrecio implements Comparator<Videojuego> {
+
+    @Override
+    public int compare(Videojuego juego1, Videojuego juego2) {
+        if (juego1.getPrecio() < juego2.getPrecio()) {
+            return -1;
+        } else if (juego1.getPrecio() > juego2.getPrecio()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
