bc5990692814c138449891d57286a63a754a59db
marti <marti@LAPTOP-LRME12KH>
2023-05-23 17:48:39 +0200

    clase mostrarJuegosDidsponibles  "muestra la lista de juegos disponibles ordenados alfabéticamente por título"

--- PracticaT8T10_Grupo3/src/videojuego/TiendaVideojuegos.java
+++ PracticaT8T10_Grupo3/src/videojuego/TiendaVideojuegos.java
@@ -1,12 +1,16 @@
 package videojuego;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class TiendaVideojuegos {
 
 	private Set<Videojuego> inventario;
-	
+
 	public TiendaVideojuegos() {
 		this.inventario = new TreeSet<>();
 	}
@@ -18,11 +22,18 @@ public class TiendaVideojuegos {
 	public void setInventario(Set<Videojuego> inventario) {
 		this.inventario = inventario;
 	}
-	
-	public void agregarVideojuego(Videojuego videoJuego) {
-	
-		inventario.add(videoJuego);
-	   
-}
 
-}
+	public void agregarVideojuego(Videojuego videoJuego) {
+
+		inventario.add(videoJuego);
+
+	}
+
+	public void mostrarJuegosDisponibles() {
+
+		System.out.println(inventario);
+
+		}
+	}
+
+
