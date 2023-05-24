612d5a9b1494a9650c57dc638d8f757642710e63
Cristina <Cristina@DESKTOP-UJ9KHU9>
2023-05-24 19:26:55 +0200

    Clase Prueba
    [Mostrar juego más caro y más barato de una tienda]

--- PracticaT8T10_Grupo3/src/prueba/Prueba.java
+++ PracticaT8T10_Grupo3/src/prueba/Prueba.java
@@ -15,8 +15,11 @@ public class Prueba {
 		
 		JuegoDigital juegoDigital1 = new JuegoDigital("GTA V", "Acción", "PC", 20.00, 50);
 		JuegoDigital juegoDigital2 = new JuegoDigital("Minecraft", "Sandbox", "PC", 19.95, 20);
+		JuegoDigital juegoDigital3 = new JuegoDigital("FIFA", "fUrBo", "PC", 60.00, 30);
 		JuegoFisico juegoFisico1 = new JuegoFisico("God of War", "Aventura", "PS4", 20.00, "NUEVO", "en_stock");
 		JuegoFisico juegoFisico2 = new JuegoFisico("Tekken", "Lucha", "PS2", 5.00, "USADO", "agotado");
+		
+		System.out.println("------------------");
 		
 		Cliente cliente1 = new Cliente("Pepe", "Calle Blasco Ibáñez, 1");
 		Cliente cliente2 = new Cliente("María", "Calle Kansas City, 2");
@@ -28,19 +31,36 @@ public class Prueba {
 		tienda2.agregarVideojuego(juegoDigital2);
 		tienda1.agregarVideojuego(juegoDigital1);
 		tienda2.agregarVideojuego(juegoFisico1);
+		tienda1.agregarVideojuego(juegoDigital3);
+		
+		System.out.println("------------------");
 		
 		tienda2.buscarJuegosPorGenero("Sandbox");
 		tienda1.buscarJuegosPorGenero("Sandbox");
 		
+		System.out.println("------------------");
+		
 		tienda2.buscarJuegosPorPlataforma("PC");
 		tienda1.buscarJuegosPorPlataforma("PS1");
+		
+		System.out.println("------------------");
 		
 		tienda1.mostrarJuegosDisponibles();
 		tienda2.mostrarJuegosDisponibles();
 		
+		System.out.println("------------------");
+		
 		tienda1.venderJuego(cliente2, juegoFisico2);
 		tienda2.venderJuego(cliente1, juegoDigital2);
 		tienda2.venderJuego(cliente1, juegoFisico2);
+		
+		System.out.println("------------------");
+	
+		Videojuego juegoMasCaro = tienda1.obtenerJuegoMasCaro();
+        System.out.println("Juego más caro: " + juegoMasCaro.getTitulo() + " - Precio: " + juegoMasCaro.getPrecio() + " €");
+        
+        Videojuego juegoMasBarato = tienda1.obtenerJuegoMasBarato();
+        System.out.println("Juego más barato: " + juegoMasBarato.getTitulo() + " - Precio: " + juegoMasBarato.getPrecio() + " €");
 		
 	}
 }
