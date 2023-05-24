3a7de0a6b01b3fae526fd17b463d2cda366d8b64
josea <josea@Mreljose>
2023-05-24 18:18:40 +0200

    Clase prueba (He creado juegos)

--- PracticaT8T10_Grupo3/src/prueba/Prueba.java
+++ PracticaT8T10_Grupo3/src/prueba/Prueba.java
@@ -9,7 +9,7 @@ public class Prueba {
 		// TODO Auto-generated method stub
 		JuegoDigital juegoDigital1 = new JuegoDigital("GTA V", "Acción", "PC", 20.00, 50);
 		JuegoDigital juegoDigital2 = new JuegoDigital("Minecraft", "Sandbox", "PC", 19.95, 20);
-		JuegoFisico juegoFisico1 = new JuegoFisico("God of War", "Aventura", "PS4", 20.00, "NUEVO", "en_stock")
+		JuegoFisico juegoFisico1 = new JuegoFisico("God of War", "Aventura", "PS4", 20.00, "NUEVO", "en_stock");
 		JuegoFisico juegoFisico2 = new JuegoFisico("Tekken", "Lucha", "PS2", 5.00, "USADO", "agotado");
 		
 		
